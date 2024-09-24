ddb68a201820937dbe9a8c3f871a3140a8711449
PolishCowPog <156892491+PolishCowPog@users.noreply.github.com>
2024-09-24 11:37:36 +0200

    Update back_end.py

--- back_end.py
+++ back_end.py
@@ -12,7 +12,15 @@ def trekkFra():
     print(f'{tall2} - {tall2} = {diff}')
 
 
-''' Her er noen påbegynte tanker til utvidelse
+''' Her er noen påbegynte tanker til utvidelse '''
 def gange():
-    produkt = 3*3
-    TODO: Gjøre ferdig '''
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    sum = int(tall1) * int(tall2)
+    print(f'{tall2} * {tall2} = {sum}')
+
+def gange():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    sum = int(tall1) / int(tall2)
+    print(f'{tall2} / {tall2} = {sum}')
